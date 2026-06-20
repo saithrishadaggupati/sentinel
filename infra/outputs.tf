1ec663f308dec7801b1aeff56aa0b3f391de92aa
Saithrishadaggupati <saithrishadaggupati@gmail.com>
2026-06-20 11:11:42 +0530

    Add Terraform IaC for Sentinel EC2 infrastructure

---
+++ infra/outputs.tf
@@ -0,0 +1,11 @@
+﻿output "instance_id" {
+  value = aws_instance.sentinel.id
+}
+
+output "public_ip" {
+  value = aws_eip.sentinel_eip.public_ip
+}
+
+output "ssh_command" {
+  value = "ssh -i ~/Downloads/${var.key_name}.pem ubuntu@${aws_eip.sentinel_eip.public_ip}"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,35 @@
+﻿variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "key_name" {
+  description = "Name of the EC2 key pair to use for SSH access"
+  type        = string
+  default     = "sentinel-key"
+}
+
+variable "project_name" {
+  description = "Name prefix used for tagging resources"
+  type        = string
+  default     = "sentinel"
+}
+
+variable "root_volume_size" {
+  description = "Root EBS volume size in GB"
+  type        = number
+  default     = 20
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed to SSH into the instance. Restrict this to your own IP in production."
+  type        = string
+  default     = "0.0.0.0/0"
+}
